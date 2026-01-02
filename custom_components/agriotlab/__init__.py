848e2ab7d6c910d92995368a0afb926dc8bc1ab9
hypertheasis <91177214+hypertheasis@users.noreply.github.com>
2026-01-02 19:35:13 +0200

    Refactor AgriotLab integration setup and unload methods

--- custom_components/agriotlab/__init__.py
+++ custom_components/agriotlab/__init__.py
@@ -1,11 +1,22 @@
-"""AgriotLab integration."""
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-DOMAIN = "agriotlab"
+from .const import DOMAIN
+
+PLATFORMS: list[str] = ["sensor"]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
-    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = entry.data
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
