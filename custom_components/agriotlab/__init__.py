e3a51cabc93ab533f9a03773bcf7ca48c987fa7f
hypertheasis <91177214+hypertheasis@users.noreply.github.com>
2026-01-02 17:33:47 +0200

    Implement async setup for AgriotLab integration

--- custom_components/agriotlab/__init__.py
+++ custom_components/agriotlab/__init__.py
@@ -1,1 +1,11 @@
+"""AgriotLab integration."""
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+
+DOMAIN = "agriotlab"
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
+    return True
