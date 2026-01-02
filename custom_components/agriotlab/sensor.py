3a977668d7d80af6490acfed8b8cd912bc16c490
hypertheasis <91177214+hypertheasis@users.noreply.github.com>
2026-01-02 17:32:06 +0200

    Add AgriotLab basic status sensor

--- custom_components/agriotlab/sensor.py
+++ custom_components/agriotlab/sensor.py
@@ -1,1 +1,22 @@
+"""AgriotLab basic sensor."""
 
+from homeassistant.components.sensor import SensorEntity
+from homeassistant.const import CONF_NAME
+
+DOMAIN = "agriotlab"
+
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    async_add_entities([AgriotLabStatusSensor()])
+
+
+class AgriotLabStatusSensor(SensorEntity):
+    """Basic status sensor for AgriotLab."""
+
+    _attr_name = "AgriotLab Status"
+    _attr_unique_id = "agriotlab_status"
+    _attr_icon = "mdi:sprout"
+
+    @property
+    def native_value(self):
+        return "ready"
